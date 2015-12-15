d3bb75c0a6057367e960b91597f67c8c32411642
Zw <knight.zhangwengame@gmail.com>
2015-12-15 18:14:43 +0800

    Fix bug: ray intersect nothing

---
+++ path_albedo.cpp
@@ -0,0 +1,200 @@
+/*
+    This file is part of Mitsuba, a physically based rendering system.
+
+    Copyright (c) 2007-2014 by Wenzel Jakob and others.
+
+    Mitsuba is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License Version 3
+    as published by the Free Software Foundation.
+
+    Mitsuba is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <mitsuba/render/scene.h>
+#include <mitsuba/core/statistics.h>
+
+MTS_NAMESPACE_BEGIN
+
+static StatsCounter avgPathLength("Path tracer", "Average path length", EAverage);
+
+/*! \plugin{path}{Path tracer}
+ * \order{2}
+ * \parameters{
+ *     \parameter{maxDepth}{\Integer}{Specifies the longest path depth
+ *         in the generated output image (where \code{-1} corresponds to $\infty$).
+ *	       A value of \code{1} will only render directly visible light sources.
+ *	       \code{2} will lead to single-bounce (direct-only) illumination,
+ *	       and so on. \default{\code{-1}}
+ *	   }
+ *	   \parameter{rrDepth}{\Integer}{Specifies the minimum path depth, after
+ *	      which the implementation will start to use the ``russian roulette''
+ *	      path termination criterion. \default{\code{5}}
+ *	   }
+ *     \parameter{strictNormals}{\Boolean}{Be strict about potential
+ *        inconsistencies involving shading normals? See the description below
+ *        for details.\default{no, i.e. \code{false}}
+ *     }
+ *     \parameter{hideEmitters}{\Boolean}{Hide directly visible emitters?
+ *        See page~\pageref{sec:hideemitters} for details.
+ *        \default{no, i.e. \code{false}}
+ *     }
+ * }
+ *
+ * This integrator implements a basic path tracer and is a \emph{good default choice}
+ * when there is no strong reason to prefer another method.
+ *
+ * To use the path tracer appropriately, it is instructive to know roughly how
+ * it works: its main operation is to trace many light paths using \emph{random walks}
+ * starting from the sensor. A single random walk is shown below, which entails
+ * casting a ray associated with a pixel in the output image and searching for
+ * the first visible intersection. A new direction is then chosen at the intersection,
+ * and the ray-casting step repeats over and over again (until one of several
+ * stopping criteria applies).
+ * \begin{center}
+ * \includegraphics[width=.7\textwidth]{images/integrator_path_figure.pdf}
+ * \end{center}
+ * At every intersection, the path tracer tries to create a connection to
+ * the light source in an attempt to find a \emph{complete} path along which
+ * light can flow from the emitter to the sensor. This of course only works
+ * when there is no occluding object between the intersection and the emitter.
+ *
+ * This directly translates into a category of scenes where
+ * a path tracer can be expected to produce reasonable results: this is the case
+ * when the emitters are easily ``accessible'' by the contents of the scene. For instance,
+ * an interior scene that is lit by an area light will be considerably harder
+ * to render when this area light is inside a glass enclosure (which
+ * effectively counts as an occluder).
+ *
+ * Like the \pluginref{direct} plugin, the path tracer internally relies on multiple importance
+ * sampling to combine BSDF and emitter samples. The main difference in comparison
+ * to the former plugin is that it considers light paths of arbitrary length to compute
+ * both direct and indirect illumination.
+ *
+ * For good results, combine the path tracer with one of the
+ * low-discrepancy sample generators (i.e. \pluginref{ldsampler},
+ * \pluginref{halton}, or \pluginref{sobol}).
+ *
+ * \paragraph{Strict normals:}\label{sec:strictnormals}
+ * Triangle meshes often rely on interpolated shading normals
+ * to suppress the inherently faceted appearance of the underlying geometry. These
+ * ``fake'' normals are not without problems, however. They can lead to paradoxical
+ * situations where a light ray impinges on an object from a direction that is classified as ``outside''
+ * according to the shading normal, and ``inside'' according to the true geometric normal.
+ *
+ * The \code{strictNormals}
+ * parameter specifies the intended behavior when such cases arise. The default (\code{false}, i.e. ``carry on'')
+ * gives precedence to information given by the shading normal and considers such light paths to be valid.
+ * This can theoretically cause light ``leaks'' through boundaries, but it is not much of a problem in practice.
+ *
+ * When set to \code{true}, the path tracer detects inconsistencies and ignores these paths. When objects
+ * are poorly tesselated, this latter option may cause them to lose a significant amount of the incident
+ * radiation (or, in other words, they will look dark).
+ *
+ * The bidirectional integrators in Mitsuba (\pluginref{bdpt}, \pluginref{pssmlt}, \pluginref{mlt} ...)
+ * implicitly have \code{strictNormals} set to \code{true}. Hence, another use of this parameter
+ * is to match renderings created by these methods.
+ *
+ * \remarks{
+ *    \item This integrator does not handle participating media
+ *    \item This integrator has poor convergence properties when rendering
+ *    caustics and similar effects. In this case, \pluginref{bdpt} or
+ *    one of the photon mappers may be preferable.
+ * }
+ */
+class MIPathTracer_albedo : public MonteCarloIntegrator {
+public:
+	MIPathTracer_albedo(const Properties &props)
+		: MonteCarloIntegrator(props) { }
+
+	/// Unserialize from a binary data stream
+	MIPathTracer_albedo(Stream *stream, InstanceManager *manager)
+		: MonteCarloIntegrator(stream, manager) { }
+
+	Spectrum Li(const RayDifferential &r, RadianceQueryRecord &rRec) const {
+		/* Some aliases and local variables */
+		const Scene *scene = rRec.scene;
+		Intersection &its = rRec.its;
+		RayDifferential ray(r);
+		Spectrum Li(0.0f);
+		bool scattered = false;
+
+		/* Perform the first ray intersection (or ignore if the
+		   intersection has already been provided). */
+		rRec.rayIntersect(ray);
+		ray.mint = Epsilon;
+		Spectrum throughput(1.0f);
+		Float eta = 1.0f;
+		if (!its.isValid()) {
+			/* If no intersection could be found, potentially return
+			radiance from a environment luminaire if it exists */
+			if ((rRec.type & RadianceQueryRecord::EEmittedRadiance)
+				&& (!m_hideEmitters || scattered))
+				Li += throughput * scene->evalEnvironment(ray);
+			return Li;
+		}
+		
+		DirectSamplingRecord dRec(its);
+
+		const BSDF *bsdf = its.getBSDF(ray);
+		if (1) {
+			
+			Spectrum value = scene->sampleEmitterDirect(dRec, rRec.nextSample2D());
+			value = Spectrum(1.0f);
+			if (!value.isZero()) {
+				const Emitter *emitter = static_cast<const Emitter *>(dRec.object);
+
+				/* Allocate a record for querying the BSDF */
+				BSDFSamplingRecord bRec(its, its.toLocal(dRec.d), ERadiance);
+
+				/* Evaluate BSDF * cos(theta) */
+				const Spectrum bsdfVal = bsdf->eval(bRec);
+
+				/* Prevent light leaks due to the use of shading normals */
+				if (!bsdfVal.isZero()) {
+
+					/* Calculate prob. of having generated that direction
+					using BSDF sampling */
+					Float bsdfPdf = bsdf->pdf(bRec);
+
+					/* Weight using the power heuristic */
+					//Float weight = miWeight(dRec.pdf, bsdfPdf);
+					Li += throughput * value * bsdfVal;
+				}
+			}
+		}
+
+		return Li;
+	}
+
+	inline Float miWeight(Float pdfA, Float pdfB) const {
+		pdfA *= pdfA;
+		pdfB *= pdfB;
+		return pdfA / (pdfA + pdfB);
+	}
+
+	void serialize(Stream *stream, InstanceManager *manager) const {
+		MonteCarloIntegrator::serialize(stream, manager);
+	}
+
+	std::string toString() const {
+		std::ostringstream oss;
+		oss << "MIPathTracer_albedo[" << endl
+			<< "  maxDepth = " << m_maxDepth << "," << endl
+			<< "  rrDepth = " << m_rrDepth << "," << endl
+			<< "  strictNormals = " << m_strictNormals << endl
+			<< "]";
+		return oss.str();
+	}
+
+	MTS_DECLARE_CLASS()
+};
+
+MTS_IMPLEMENT_CLASS_S(MIPathTracer_albedo, false, MonteCarloIntegrator)
+MTS_EXPORT_PLUGIN(MIPathTracer_albedo, "MI path tracer");
+MTS_NAMESPACE_END
